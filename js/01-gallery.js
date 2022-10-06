6431b6ff8311864ed7a420ee4d80b1766fb7b3f8
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-06 14:54:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+// console.log(galleryContainer);
+const makrupGallery = createGallaryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", makrupGallery)
+
+function createGallaryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+        })
+        .join('');
+}
