541d00bfa1d2f08f800a64a719072ed31fa0ff5a
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-06 15:56:58 +0300

    ex1 stack2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,17 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery')
-// console.log(galleryContainer);
 const makrupGallery = createGallaryCardsMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML("afterbegin", makrupGallery)
+
+
+
+galleryContainer.addEventListener('click', onClickUrlBigImage);
+
 
 function createGallaryCardsMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
             return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
+        <div class="gallery__item" href = "${original}">
+            <a class="gallery__link" >
                 <img
                 class="gallery__image"
                 src="${preview}"
@@ -24,4 +28,24 @@ function createGallaryCardsMarkup(galleryItems) {
         `;
         })
         .join('');
+
 }
+
+function onClickUrlBigImage(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    let source = e.target.dataset.source;
+    // console.log(source);
+    // return source;
+
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`)
+    instance.show()
+
+}
+
+
+
+
