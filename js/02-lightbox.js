f0d96c738f23f059d7889b71c204c4cdfe3ef02b
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-08 15:03:50 +0300

    ex 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const makrupGallery = createGallaryCardsMarkup(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML("afterbegin", makrupGallery)
+galleryContainer.addEventListener('click', onClickUrlBigImage);
+
+
+function createGallaryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+        })
+        .join('');
+}
+
+
+function onClickUrlBigImage(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+        alertErrorMessage: 'Изображение не найдено, простите сейчас загрузим следующее изображение'
+
+    });
+    gallery.on('show.simplelightbox', function () {
+
+    });
+
+}
+
+
+
+
